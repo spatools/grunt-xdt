e2eba44641c63e0a0533769c1a8c79aaa4cc51b1
SomaticIT <contact@somatic.fr>
2015-03-08 09:23:07 +0100

    Add new tests to match multiple files scenarios

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@
  * Licensed under the MIT license.
  */
 
-'use strict';
+"use strict";
 
 module.exports = function(grunt) {
 
@@ -14,33 +14,50 @@ module.exports = function(grunt) {
   grunt.initConfig({
     jshint: {
       all: [
-        'Gruntfile.js',
-        'tasks/*.js'
+        "Gruntfile.js",
+        "tasks/*.js"
       ],
       options: {
-        jshintrc: '.jshintrc',
+        jshintrc: ".jshintrc",
       },
     },
 
     // Configuration to be run (and then tested).
     xdt: {
         debug: {
-            src: 'test/Web.config',
-            dest: 'test/Web.Result.config',
+            src: "test/Web.config",
+            dest: "test/results/Web.Result.config",
             options: {
-                transform: 'test/Web.Release.config',
+                transform: "test/Web.Release.config",
+            }
+        },
+        test: {
+            src: "test/Web2.config",
+            dest: "test/results/Web2.Result.config",
+            options: {
+                transform: "test/Web2.Release.config",
+            }
+        },
+        all: {
+            src: [
+                "test/Web.config",
+                "test/Web2.config"
+            ],
+            dest: "test/results/",
+            options: {
+                transform: "test/Web.Release.config",
             }
         }
     }
   });
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+  // Actually load this plugin"s task(s).
+  grunt.loadTasks("tasks");
 
   // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks("grunt-contrib-jshint");
 
   // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'xdt']);
+  grunt.registerTask("default", ["jshint", "xdt"]);
 
 };
